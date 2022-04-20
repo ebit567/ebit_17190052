da838290840776c020a32853dc6a76e182862dcc
ebit567 <ebitwasis86@gmail.com>
2022-04-21 01:58:56 +0700

    first commit

---
+++ WP2_17190052/application/controllers/contoh_1.php
@@ -0,0 +1,12 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "<h1>BIODATA</h1>";
+        echo "Nama : Ebit Nurruly Wasis<br>";
+        echo "Kelas : 17.6A.01 <br>";
+        echo "NIM : 17190052<br>";
+        echo "Jenis Kelamin : Laki-Laki";
+	}
+}
